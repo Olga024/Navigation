8b80886a9cfc6db89ff5dc545b8bdeaa76f17f4a
olga <olga@local>
2025-11-05 23:51:02 +0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Router } from 'react-router-dom';
 import './App.css'
+import { Menu } from './components/Menu';
+import { DriftPage } from './components/DriftPage';
+import { ForzaPage } from './components/ForzaPage';
+import { HomePage } from './components/HomePage';
+import { TimeAttackPage } from './components/TimeAttackPage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <Router location={''} navigator={undefined}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" Component={DriftPage} />
+          <Route path="/timeattack" Component={TimeAttackPage} />
+          <Route path="/forza" Component={ForzaPage} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
-
-export default App
